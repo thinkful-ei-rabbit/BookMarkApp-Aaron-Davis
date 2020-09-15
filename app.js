a2f4d577ee52cd834d5f5fbb5319abf73768d97d
Aaron Davis <63382332+AD30344@users.noreply.github.com>
2020-09-15 10:29:09 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const path = require('path');
-
-//app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(__dirname, 'src'))); //  "public" off of current is root
-
-app.listen(80);
-console.log('Listening on port 80');
